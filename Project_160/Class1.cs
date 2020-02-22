75117ef2c9812894f53efd9d2d03593a2a173467
Kaley <kaleyrennie14@gmail.com>
2020-02-21 19:18:12 -0800

    Add project files.

---
+++ Project_160/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project160
+{
+    public class Parameters
+    {
+        //public int AddNumber(int x)
+        //{
+        //    return x;
+        //}
+        public int AddNumber(int x, int y = 10) //optional b.c default number
+        {
+            return x + y; //if one number given, will apply value of 10 to y
+        }
+
+
+    }
+}
---
+++ Project_160/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Project160
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Parameters objectParameters = new Parameters(); //instantiate
+
+            Console.WriteLine("Please enter fist number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int newVar1; //creating variable
+
+            Console.WriteLine("Please enter second number(not needed.)");
+            string y = Console.ReadLine();
+
+            if (y == "")
+            {
+                newVar1 = objectParameters.AddNumber(x);
+            }
+            else
+            {
+                int intY = Convert.ToInt32(y);
+                newVar1 = objectParameters.AddNumber(x, intY);
+            }
+
+            Console.WriteLine(newVar1);
+
+
+
+        }
+    }
+}
